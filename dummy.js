4fb53f2af217374b6bc69618ebaf7bf782bb8a82
SAYYED MOHD ARIF <122776196+mrmohdarif@users.noreply.github.com>
2023-09-05 22:25:58 +0530

    Add files via upload

---
+++ dummy.js
@@ -0,0 +1,8 @@
+const datadummy=[
+{
+    "nodejs":"what is Node JS!! ",
+    "nodejs_defination":"what is Node JS!! NodeJS is the runtime environment, which can execute the javascript code on any platform. It is widely used to create and run web application servers because of its salient features. During production, several times we need to send the resources or some type of information as a response, and javascript object notation (JSON) syntax is widely used to send data also it is used for communication between any two applications. In this article, we are going to see how we can send information to the user as JSON through a node.js server. NodeJS contains an inbuilt HTTP module, it is used to transfer data over the HTTP protocol and supports many features that are useful for any web application.</p><h2>Fast and scalable: Node.js is built on the V8 JavaScript engine, which is known for its speed and efficiency. It is designed to handle a large number of concurrent connections and can handle high traffic volumes without sacrificing performance.</h2><h3>Non-blocking I/O: Node.js uses an event-driven, non-blocking I/O model, which means that it can handle multiple requests simultaneously without blocking the execution of other requests. This makes it highly efficient for handling real-time applications and streaming data "
+}
+]
+
+exports.module=datadummy
